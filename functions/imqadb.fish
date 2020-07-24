79fa554172c2420329042fbdac680b7c8bf2442b
Tony Spataro <xeger@xeger.net>
2020-07-24 15:11:26 -0700

    ts - launch TablePlus directly from imqadb

--- functions/imqadb.fish
+++ functions/imqadb.fish
@@ -8,14 +8,14 @@ function imqadb
   echo "Forwarding 127.0.0.0:13306 via $eks_cluster deployment/$eks_proxy (cross check against qa_db_connect.sh - it may have changed)"
   aws eks --region us-east-2 update-kubeconfig --name $eks_cluster
   set -l password (aws rds generate-db-auth-token --hostname $rds_cluster --port 3306 --region us-east-2 --username im_operators)
+  set -l password_urlencoded (echo $password | ruby -rcgi -e "STDOUT.write(CGI.escape(STDIN.readline.chomp))")
   kubectl --namespace=default port-forward deployment/$eks_proxy 13306:3306 &
-  set -x LIBMYSQL_ENABLE_CLEARTEXT_PLUGIN 1
-  echo "--- BEGIN OBNOXIOUSLY LONG MYSQL PASSWORD ---"
-  echo $password
-  echo "--- END OBNOXIOUSLY LONG MYSQL PASSWORD ---"
-  echo
   sleep 3
+
   echo "Connecting to $rds_cluster (cross check against qa_db_connect.sh - it may have changed)"
-  mysql --ssl-ca=~/Documents/rds-ca-2019-root.pem  --host=127.0.0.1 --port=13306 --enable-cleartext-plugin --user=im_operators --password="$password"
+  set -x LIBMYSQL_ENABLE_CLEARTEXT_PLUGIN 1
+  open -W "mysql://im_operators:$password_urlencoded@127.0.0.1:13306"
+  # mysql --ssl-ca=~/Documents/rds-ca-2019-root.pem  --host=127.0.0.1 --port=13306 --enable-cleartext-plugin --user=im_operators --password="$password"
+
   kill (jobs -l | grep -E "kubectl.*port-forward.*deployment/.eks_proxy" | awk '{print $2}')
 end
