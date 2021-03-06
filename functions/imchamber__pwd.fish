76104d78c534e816ea11bf213392914328e0952f
Tony Spataro <xeger@xeger.net>
2021-03-06 11:40:24 -0800

    Fix chamber command.

--- functions/imchamber__pwd.fish
+++ functions/imchamber__pwd.fish
@@ -1,7 +1,11 @@
 function imchamber__pwd --on-variable=PWD
   if test -f .chamber
     if test "$AWS_ACCOUNT_ID" = "410444354559"
-      imchamber
+      if test (math $AWS_SESSION_EXPIRY - (jq -n now)) -gt 60
+        imchamber
+      else
+        echo "dotfiles: skipping imchamber pwd hook (expired AWS credentials)"
+      end
     else
       echo "dotfiles: skipping imchamber pwd hook (no AWS credentials)"
     end
