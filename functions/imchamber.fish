1b55d79148d1264c5c90fc8fa4223510177e1c57
Tony Spataro <xeger@xeger.net>
2021-04-28 17:16:09 -0700

    Add more flourishes to imchamber

--- functions/imchamber.fish
+++ functions/imchamber.fish
@@ -1,5 +1,5 @@
 function imchamber
-  argparse --name=imchamber 'h/help' 'f/file' -- $argv 2> /dev/null
+  argparse --name=imchamber 'h/help' -- $argv 2> /dev/null
   or set _flag_h 1
 
   set -l argcount (count $argv)
@@ -28,41 +28,33 @@ function imchamber
     return 1
   end
 
-  if test -n "$_flag_f"
-    rm -f .env.local
-    touch .env.local
-  end
-
   if test $legacy_format = true
     echo "imchamber: using legacy .chamber file"
     for service in $services
       if test -n "$service"
-        if test -n "$_flag_f"
-          echo "imchamber: adding $service to your .env.local"
-          chamber export --format=dotenv $service >> .env.local
-        else
-          echo "imchamber: exporting $service to your shell"
-          set -l secrets_json (chamber export --format=json $service)
-          set -l secrets_names (echo $secrets_json | jq -r 'keys | .[]')
-          for name in $secrets_names
-            set -gx (echo $name | tr a-z A-Z)  (echo $secrets_json | jq -r .$name)
-          end
+        echo "imchamber: exporting $service to your shell"
+        set -l secrets_json (chamber export --format=json $service)
+        set -l secrets_names (echo $secrets_json | jq -r 'keys | .[]')
+        for name in $secrets_names
+          set -gx (echo $name | tr a-z A-Z)  (echo $secrets_json | jq -r .$name)
         end
       end
     end
   else
     for cmd in $services
-      echo "imchamber: $cmd"
-      if string match -qr '^env' $cmd
-        set -l cmd_suffix (string sub -s5 $cmd)
-        set -l secrets_json (chamber export --format=json $cmd_suffix)
-        set -l secrets_names (echo $secrets_json | jq -r 'keys | .[]')
-        for name in $secrets_names
-          set -gx (echo $name | tr a-z A-Z)  (echo $secrets_json | jq -r .$name)
+      if not string match -qr '^#|^$' $cmd # ignore comments and empty lines
+        echo "imchamber: $cmd"
+        if string match -qr '^env' $cmd
+          set -l cmd_suffix (string sub -s5 $cmd)
+          set -l secrets_json (chamber export --format=json $cmd_suffix)
+          set -l secrets_names (echo $secrets_json | jq -r 'keys | .[]')
+          for name in $secrets_names
+            set -gx (echo $name | tr a-z A-Z)  (echo $secrets_json | jq -r .$name)
+          end
+        else
+          set -l entire_cmd "chamber $cmd"
+          eval $entire_cmd
         end
-      else
-        set -l entire_cmd "chamber $cmd"
-        eval $entire_cmd
       end
     end
   end
