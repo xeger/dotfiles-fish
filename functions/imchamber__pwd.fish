e17def4822b0a0e6c865bac827f5f71922c5e4a4
Tony Spataro <xeger@xeger.net>
2021-07-01 12:17:51 -0700

    Make imchamber useful for non-dev accounts

--- functions/imchamber__pwd.fish
+++ functions/imchamber__pwd.fish
@@ -1,6 +1,6 @@
 function imchamber__pwd --on-variable=PWD
-  if test -f .chamber
-    if test "$AWS_ACCOUNT_ID" = "410444354559"
+  if test -f .chamberrc
+    if test -n "$AWS_ACCOUNT_ID"
       if test (math $AWS_SESSION_EXPIRY - (jq -n now)) -gt 60
         imchamber
       else
