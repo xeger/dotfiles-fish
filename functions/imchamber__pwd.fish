4fc55bf10a46579648d00d7836e9c989c4948702
Tony Spataro <anthony.spataro@appfolio.com>
2022-05-11 10:57:15 -0700

    Fix fswatch; skip imchamber on redo

--- functions/imchamber__pwd.fish
+++ functions/imchamber__pwd.fish
@@ -1,8 +1,14 @@
 function imchamber__pwd --on-variable=PWD
   if test -f .chamberrc
+    set -l fingerprint (md5 -q .chamberrc)
     if test -n "$AWS_ACCOUNT_ID"
       if test (math $AWS_SESSION_EXPIRY - (jq -n now)) -gt 60
-        imchamber
+        if test "$fingerprint" != "$IMCHAMBER_PWD_LAST_FINGERPRINT"
+          imchamber
+          set -gx IMCHAMBER_PWD_LAST_FINGERPRINT $fingerprint
+        else
+          echo "dotfiles: skipping imchamber pwd hook (already ran)"
+        end
       else
         echo "dotfiles: skipping imchamber pwd hook (expired AWS credentials)"
       end
