aaeaf312d1160d876c85336ddc6c42611235030d
Tony Spataro <anthony.spataro@appfolio.com>
2022-06-16 09:57:50 -0700

    Prioritize Makefile over language-specific task files

--- functions/r.fish
+++ functions/r.fish
@@ -1,6 +1,8 @@
 # Run a script/task of a Ruby or Javascript project located in PWD.
 function r
-  if test -f Rakefile
+  if test -f Makefile
+    make $argv
+  else if test -f Rakefile
     bundle exec rake $argv
   else if test -f package.json
     set -l pmgr (cat package.json | jq -r .packageManager)
@@ -10,8 +12,6 @@ function r
       case '*'
         npm run $argv
     end
-  else if test -f Makefile
-    make $argv
   else
     echo "r: Nothing seems to be runnable in this folder"
   end
