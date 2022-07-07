cca82d20db56f1178d8271873e2a7a50e1cb408a
Tony Spataro <anthony.spataro@appfolio.com>
2022-07-07 16:22:31 -0700

    Fix minor bugs

--- functions/ci.fish
+++ functions/ci.fish
@@ -1,7 +1,7 @@
 # Open a browser displaying the continuous integration status of the active branch.
 function ci
   set -l root (git rev-parse --show-toplevel)
-  set -l remote (git remote -v | grep push | sed 's|^.*github.com[:/]\(.*\)\.git.*$|\1|')
+  set -l remote (git remote -v | grep -E '^origin.*push' | sed 's|^.*github.com[:/]\(.*\)\.git.*$|\1|')
   set -l org (echo $remote | cut -d/ -f1)
   set -l repo (echo $remote | cut -d/ -f2)
   set -l branch (git symbolic-ref HEAD 2>/dev/null | sed -e 's:refs/heads/::')
