679336faef50361c5702a28f90b77cdf4907b93c
Tony Spataro <anthony.spataro@appfolio.com>
2023-01-19 10:28:57 -0500

    Fix imchamber/imaws bugaboos

--- functions/imchamber.fish
+++ functions/imchamber.fish
@@ -7,8 +7,13 @@ function imchamber
   set -l services ""
 
   if test $argcount -eq 0; and test -f .chamberrc
-    set fingerprint "$AWS_ACCOUNT_ID"(md5 -q .chamberrc)
-    set services (cat .chamberrc)
+    if test -n "$AWS_ACCOUNT_ID"
+      set fingerprint "$AWS_ACCOUNT_ID"(md5 -q .chamberrc)
+      set services (cat .chamberrc)
+    else
+      echo "imchamber: skipping (no AWS credentials available); use --force to override"
+      return 0
+    end
   else if test $argcount -gt 0
     for service in $argv
       set -a services "env $service"
