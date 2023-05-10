a94b2cfee1606a10ad7e6710a89fc556795387c0
Tony Spataro <anthony.spataro@appfolio.com>
2023-05-10 09:12:25 -0700

    Improve i/x/w

--- functions/i.fish
+++ functions/i.fish
@@ -1,8 +1,10 @@
 # Install Ruby/Javascript project dependencies in PWD.
 function i
-  if test -f Gemfile.lock
-    bundle install $argv
-  else if test -f package.json
+  if test -f Gemfile
+    bundle check || bundle install
+  end
+
+  if test -f package.json
     set -l pmgr (cat package.json | jq -r .packageManager)
     switch $pmgr
       case 'npm*'
@@ -10,10 +12,15 @@ function i
       case 'yarn*'
         yarn install
       case '*'
-        echo "i: Dunno how to install; please add packageManager to package.json"
-        return 1
+        if test -f package-lock.json
+          npm install
+        else if test -f yarn.lock
+          yarn install
+        else
+          echo "i: Dunno how to install; please add packageManager to package.json"
+          return 1
+        end
     end
-  else
-    echo "i: Nothing seems to be installable in this folder"
   end
 end
+
--- functions/x.fish
+++ functions/x.fish
@@ -1,10 +1,23 @@
-# Run a project-local Ruby/Javascript executable in the PWD.
+# Run a project-local Ruby/JavaScript executable in the PWD.
 function x
-  if test -f Gemfile.lock; or test (count *.gemspec) -gt 0
+  if test -f Gemfile; or test (count *.gemspec) -gt 0
     bundle exec $argv
-  else if test -f yarn.lock
-    yarn $argv
-  else if test -f package-lock.json
-    npx $argv
+  else if test -f package.json
+    set -l pmgr (cat package.json | jq -r .packageManager)
+    switch $pmgr
+      case 'npm*'
+        npx $argv
+      case 'yarn*'
+        yarn exec $argv
+      case '*'
+        if test -f package-lock.json
+          npx $argv
+        else if test -f yarn.lock
+          yarn $argv
+        else
+          echo "i: Dunno how to run; please add packageManager to package.json"
+          return 1
+        end
+    end
   end
 end
