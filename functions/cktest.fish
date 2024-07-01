833afe871d982a07aff814b43c6da2ef41c9058a
Tony Spataro <tony@crossnokaye.com>
2024-07-01 12:03:42 -0700

    Fix cktest issue

--- functions/cktest.fish
+++ functions/cktest.fish
@@ -3,17 +3,17 @@ function cktest
     case 'env'
       echo "Populate environment with test credentials:"
       echo "  test @ ck - login all envs"
-      set -x TEST_USERNAME (op read op://Shared/ln6etqriqm5ahcqdan62zvl52e/username)
-      set -x TEST_PASSWORD (op read op://Shared/ln6etqriqm5ahcqdan62zvl52e/password)
+      set -gx TEST_USERNAME (op read op://Shared/ln6etqriqm5ahcqdan62zvl52e/username)
+      set -gx TEST_PASSWORD (op read op://Shared/ln6etqriqm5ahcqdan62zvl52e/password)
       echo "  test+admin @ ck - login all envs"
-      set -x TEST_ADMIN_USERNAME (op read op://Shared/7v2njpe4hfi5dxtkmrzm5qokju/username)
-      set -x TEST_ADMIN_PASSWORD (op read op://Shared/7v2njpe4hfi5dxtkmrzm5qokju/password)
+      set -gx TEST_ADMIN_USERNAME (op read op://Shared/7v2njpe4hfi5dxtkmrzm5qokju/username)
+      set -gx TEST_ADMIN_PASSWORD (op read op://Shared/7v2njpe4hfi5dxtkmrzm5qokju/password)
       echo "  test+operator @ ck - login all envs"
-      set -x TEST_OPERATOR_USERNAME (op read op://Shared/fdtbpdq7sv3ardyg3r6iyzsui4/username)
-      set -x TEST_OPERATOR_PASSWORD (op read op://Shared/fdtbpdq7sv3ardyg3r6iyzsui4/password)
+      set -gx TEST_OPERATOR_USERNAME (op read op://Shared/fdtbpdq7sv3ardyg3r6iyzsui4/username)
+      set -gx TEST_OPERATOR_PASSWORD (op read op://Shared/fdtbpdq7sv3ardyg3r6iyzsui4/password)
       echo "  test+view @ ck - login all envs"
-      set -x TEST_VIEW_USERNAME (op read op://Shared/nqpltj5fax4mygkvf7fvsz7r7e/username)
-      set -x TEST_VIEW_PASSWORD (op read op://Shared/nqpltj5fax4mygkvf7fvsz7r7e/password)
+      set -gx TEST_VIEW_USERNAME (op read op://Shared/nqpltj5fax4mygkvf7fvsz7r7e/username)
+      set -gx TEST_VIEW_PASSWORD (op read op://Shared/nqpltj5fax4mygkvf7fvsz7r7e/password)
     case 'dump-secret'
       echo "Dump Kubernetes secret $argv[3] of namespace $argv[2] to stdout"
       kubectl -n $argv[2] get secret $argv[3] -o jsonpath='{.data}' | jq '.|map_values(@base64d)'
