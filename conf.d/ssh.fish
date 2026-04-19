269ab25afb54587b840ee49d1ff4a5a2eb2fe033
Tony Spataro <tony@crossnokaye.com>
2026-04-19 14:49:35 -0700

    fix: zero base case

--- conf.d/ssh.fish
+++ conf.d/ssh.fish
@@ -1,6 +1,10 @@
 if test -n "$SSH_AUTH_SOCK"
-  set -l private_keys (grep -r --include=\* -El 'BEGIN [A-Z]* ?PRIVATE KEY' ~/.ssh/id_*)
-  if test $status = 0; and test -n "$private_keys"
+  set -l key_candidates (find ~/.ssh -maxdepth 1 -type f -name 'id_*' 2>/dev/null)
+  set -l private_keys
+  if test -n "$key_candidates"
+    set private_keys (grep -lE 'BEGIN [A-Z]* ?PRIVATE KEY' $key_candidates 2>/dev/null)
+  end
+  if test -n "$private_keys"
     ssh-add $private_keys 2> /dev/null
     if test $status != 0
       echo "Could not add private SSH keys to agent"
